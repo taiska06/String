184f002e7138ca79b416b85115eb4708cae9ec95
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-11-20 13:45:48 +0300

    Add files via upload

---
+++ str_easy.h
@@ -0,0 +1,13 @@
+#ifndef STR_EASY_H_INCLUDED
+#define STR_EASY_H_INCLUDED
+#include <iostream>
+#include<string>
+using namespace std;
+string itc_hello_str(string name);
+long long itc_len(string str);
+void itc_print_copy_str(string str, int number);
+void itc_first_end_three(string str);
+int itc_count_char_in_str(char ch, string str);
+string itc_even_place(string str);
+
+#endif // STR_EASY_H_INCLUDED
---
+++ strin.cpp
@@ -0,0 +1,70 @@
+#include "str_easy.h"
+using namespace std;
+string itc_hello_str(string name){
+    return ("Hello,"+ name);
+
+}
+ long long itc_len(string str){
+     int kol,i;
+     kol=0;
+     i=0;
+     while(str [i]!= '\0'){
+      kol++;
+      i++;
+ }
+ return kol;
+ }
+void itc_print_copy_str(string str, int number){
+int i=0;
+while(i<number){
+    i++;
+    cout<<str;
+}
+}
+void itc_first_end_three(string str){
+    int length = itc_len(str);
+    if(length<5){
+        int a=0;
+        while(a<length){
+            a++;
+            cout<<str[0];
+        }
+    }else{
+      cout<< str[0];
+      cout<< str[1];
+      cout<< str[2];
+      cout<< str[length - 3];
+      cout<< str[length - 2];
+      cout<< str[length - 1];
+    }
+
+}
+int itc_count_char_in_str(char ch, string str){
+int i=0;
+int c=0;
+int f=0;
+int length = itc_len(str);
+while(i<length){
+
+   if(str[c]==ch){
+    f++;
+   }
+    c++;
+    i++;
+}
+return f;
+}
+string itc_even_place(string str){
+int length = itc_len(str);
+int c=0;
+int i=0;
+while(i<length){
+if(c%2==0){
+    cout<<str[c];
+   }
+i++;
+c++;
+}
+}
+
+
