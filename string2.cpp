2b94424dd8fceb0252008a0d85c4acdb0d96ec3f
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-12-01 17:38:19 +0300

    Update string2.cpp

--- string2.cpp
+++ string2.cpp
@@ -1,28 +1,29 @@
 #include "str_easy.h"
 using namespace std;
-string itc_even_place(string str){
-int length = itc_len(str);
-int c=0;
-for(int i=0;i<length;i++){
-if(i%2==0){
-    cout<<str[i];
-   }
+string itc_even_place(string str) {
+long long len = itc_len(str);
+if (len < 2){
+return "-1";
 }
-string itc_reverse_str(string str){
-int length = itc_len(str);
-for(int i=length;i>-1;i--){
-    cout<<str[i];
+string a;
+for (long long i = 1; i < len; i=i+2){
+a = a+str[i];
 }
+return a;
 }
- string itc_slice_str(string str, int start, int end){
- int length = itc_len(str);
- int i=0;
-while(i<start){
-    i++;
-    }
-while(i<end){
-    cout<<str[i];
-    i++;
-    }
+double itc_percent_lower_uppercase(string str){
+    int a=0;
+    int b=0;
+    int i=0;
+ while(str [i]!= '\0'){
 
+      if(str[i]>='A' && str[i]<='Z'){
+        a++;
+      }
+      if(str[i]>='a' && str[i] <='z'){
+        b++;
+      }
+      i++;
+ }
+return (a/b)*100;
 }
