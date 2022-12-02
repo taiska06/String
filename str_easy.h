048dc6202fbecd517ee1e4584979219396303bd1
taiska06 <112109525+taiska06@users.noreply.github.com>
2022-12-02 11:43:43 +0300

    Update str_easy.h

--- str_easy.h
+++ str_easy.h
@@ -11,5 +11,6 @@ int itc_count_char_in_str(char ch, string str);
 string itc_even_place(string str);
 double itc_percent_lower_uppercase(string str);
 string itc_reverse_str(string str);
-string  itc_slice_str(string str, int start, int end)
+string  itc_slice_str(string str, int start, int end);
+bool itc_equal_reverse(string str);
 #endif // STR_EASY_H_INCLUDED
